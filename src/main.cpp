6b64aaecb1fb33b6d67694c8cbb3586f7964e700
SebiTCR <Ssebi832@outlook.com>
2022-03-29 06:40:57 +0300

    Added overall data serialization

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,47 @@
 #include <string.h>
 #include <Thread.h>
 
+DynamicJsonDocument iBuff(1024);
+
 ModPort firstMod(1, 22);
-ModPort secoundMod(1, 24);
+ModPort secoundMod(2, 24);
 Screen  screen;
 
 Thread FMTh = Thread();
 Thread SMTh = Thread();
-
+Thread buffInterface = Thread();
 
 void updateFM(){firstMod.updateMod();}
 void updateSM(){secoundMod.updateMod();}
 
+void updateIBuff(){
+    String buffer = "";
+    iBuff["Modules"]["M1"]["isConnected"] = firstMod.isConnected();
+    iBuff["Modules"]["M1"]["Measurement"] = firstMod.type;
+    iBuff["Modules"]["M1"]["Value"]       = firstMod.value;
+
+    iBuff["Modules"]["M2"]["isConnected"] = secoundMod.isConnected();
+    iBuff["Modules"]["M2"]["Measurement"] = secoundMod.type;
+    iBuff["Modules"]["M2"]["Value"]       = secoundMod.value;
+
+    
+    serializeJson(iBuff, buffer);
+    Serial.println(buffer);
+}
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial1.begin(115200);
-    Serial2.begin(9600);
+    Serial2.begin(115200);
 
     FMTh.onRun(updateFM);
     FMTh.setInterval(500);
 
     SMTh.onRun(updateSM);
     SMTh.setInterval(500);
+
+    buffInterface.onRun(updateIBuff);
+    buffInterface.setInterval(1000);
 
     // firstMod   = new ModPort;
     // secoundMod = new ModPort;
@@ -36,22 +55,23 @@ void loop() {
 
     if(firstMod.isConnected()){
         if(FMTh.shouldRun()){FMTh.run();}
-        screen.print("Mod 1: " + String(firstMod.value) + String(" ?u"), 0);
+        screen.print("Mod 1: " + String(firstMod.value) + firstMod.type, 0);
+
     }
-    else
-    {
+    else{
         screen.clear(0);
     }
 
-    if(secoundMod.isConnected())
-    {
-        if(SMTh.shouldRun()){SMTh.run();}
-        screen.print("Mod 2: " + String(secoundMod.value) + String(" ??"), 1);
+    if(secoundMod.isConnected()){
+       if(SMTh.shouldRun()){SMTh.run();}
+       screen.print("Mod 2: " + String(secoundMod.value) + secoundMod.type, 1);
     }
-    else
-    {
-        screen.clear(1);
+    else{
+       screen.clear(1);
     }
+
+    if(buffInterface.shouldRun()){buffInterface.run();}
+
 
     //if(Serial1.available()){Serial.println(Serial1.read());}
 }
